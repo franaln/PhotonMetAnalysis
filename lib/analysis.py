4e7a8d0e80574e5c213e1b9ef4c4c37f960cbf20
franaln <franaln@gmail.com>
2016-11-24 22:30:39 +0100

    add new lumi, new samples

--- lib/analysis.py
+++ lib/analysis.py
@@ -2,23 +2,16 @@
 use_purw = False
 
 # Mini version
-versions = ['41', ]
+versions = ['45', '41',] # keep v41 for signal
 
-ptags = ['2689', '2688', '2666', '2667', '2622', '2614']
+ptags = ['2840', '2824', '2769', '2689', '2688', '2666', '2667', ] #'2622', '2614']
 
 
-# Luminosity
-# lumi_data15 = 3209.05 # 20.1 
-# lumi_data15 = 3245.43 # pb-1 20.7
-# lumi_data15 = 3193.68 # pb-1 20.7 FINAL GRL v79-repro20-02 
-# lumi_data16 = 2613.83 # pb-1 DS1
-# lumi_data16 = 6302.24 # pb-1 DS1.2
-# lumi_data16 = 8585.76 # pb-1 DS1.3
-
-# NEW: OffLumi-13TeV-005
+# Luminosity (OffLumi-13TeV-005)
 lumi_data15 = 3212.96  # All
-lumi_data16 = 10064.30 # DS2
-#lumi_data16 = 11571.50 # DS2.1
+#lumi_data16 = 10064.30 # DS2 (Runs 297730-303560)
+#lumi_data16 = 24799.90 # DS3 (Runs 297730-308084)
+lumi_data16 = 33257.20 # All 2016
 
 lumi_data = lumi_data15 + lumi_data16
 
@@ -69,8 +62,9 @@ cr_regions = [
 
 vr_regions = [
     'VRM1', 'VRM2', 'VRM3',
-    'VRD1', 'VRD2', 'VRD3',
-    'VRL1', 'VRL2', 'VRL3', 'VRL4'
+    'VRE1', 'VRE2', 'VRE3',
+    'VRL1', 'VRL2', 'VRL3', 'VRL4',
+    'VRZ',
     ]
 
 
