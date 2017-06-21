962053fbcd881626a1d9d32f12c5506d42c31adf
franaln <franaln@gmail.com>
2017-06-21 11:54:22 +0200

    fix memory leackeage in merge_hypotest.py

--- scripts/merge_hypotest.py
+++ scripts/merge_hypotest.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 
 import os
+import re
 import sys
 import fnmatch
 import ROOT
 
 ROOT.gSystem.Load("libSusyFitter.so")
 
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    return [ atoi(c) for c in re.split('(\d+)', text) ]
+
 def merge(input_files, output_file):
     
-    """ merge all files in input_files into output_file """
+    """ merge the hypotest results and fits from all
+    files in input_files into output_file """
 
-    outfile = ROOT.TFile(output_file, 'recreate')
-
-    hypo_dict = dict()
-    fit_dict  = dict()
-
+    # Get all available points
+    all_points = []
     for path in input_files:
 
         infile = ROOT.TFile.Open(path)
@@ -23,54 +28,77 @@ def merge(input_files, output_file):
         content = [ key.GetName() for key in infile.GetListOfKeys() ]
 
         for name in content:
-
             if not name.startswith('hypo'): ## or nor name.startswith('fitTo'):
                 continue
+            all_points.append(name.replace('hypo_', ''))
 
-            point = name.replace('hypo_', '')
+        infile.Close()
 
-            hypotest = infile.Get(name)
+    all_points = list(set(all_points))
 
-            if not hypotest or hypotest.ClassName() != 'RooStats::HypoTestInverterResult':
+    all_points.sort(key=natural_keys)
+
+    # Loop over points, merge them and save it to output file
+    for point in all_points:
+
+        print 'Merging point: %s' % point
+
+        name = 'hypo_%s' % point
+
+        merge_hypotest = None
+        merge_fit      = None
+
+        for path in input_files:
+            infile = ROOT.TFile.Open(path)
+
+            try:
+                hypotest = infile.Get(name)
+            except:
+                hypotest = None
+            
+            if not hypotest or hypotest is None or hypotest.ClassName() != 'RooStats::HypoTestInverterResult':
                 continue
             
-            if not point in hypo_dict:
-                hypo_dict[point] = hypotest
+            if merge_hypotest is None:
+                merge_hypotest = hypotest.Clone()
             else:
-                hypo_dict[point].Add(hypotest)
+                merge_hypotest.Add(hypotest.Clone())
+
+            hypotest.Delete()
 
             # Corresponding fit
-            try:
-                fit_dict[point] = infile.Get(name.replace('hypo', 'fitTo'))
-            except:
-                print 'No fit available for point: %s' % point
+            if merge_fit is None:
+                try:
+                    merge_fit = infile.Get(name.replace('hypo', 'fitTo')).Clone()
+                except:
+                    print 'No fit available for point: %s' % point
 
+            infile.Close()
 
-    # Save objects in hypo/fit dicts
-    outfile.cd()
-    for point in hypo_dict.itervalues():
-        point.Write()
-    for point in fit_dict.itervalues():
-        point.Write()
-    outfile.Close()
+        # save point to output file
+        outfile = ROOT.TFile(output_file, 'update')
+        outfile.cd()
+        merge_hypotest.Write()
+        merge_fit.Write()
+        outfile.Close()
+
+        
 
 
 ## Main
-if len(sys.argv) < 4:
-    print 'usage: merge_hypotest.py    output_dir input_dir1 input_dir2 ...'
-    print '       merge_hypotest.py -r output_dir input_dir' 
-
+if len(sys.argv) < 3:
+    print 'usage: merge_hypotest.py output_dir input_dir1 input_dir2 ...'
     sys.exit(1)
 
     
 files_nominal = []
 files_xsec_dn = []
 files_xsec_up = []
-if sys.argv[1] == '-r':
 
-    output_dir = sys.argv[2]
-    input_dir  = sys.argv[3]
+output_dir = sys.argv[1]  
+input_dirs  = sys.argv[2:] 
 
+for input_dir in input_dirs:
     for root, dirnames, filenames in os.walk(input_dir):
         for filename in fnmatch.filter(filenames, 'Output_fixSigXSecNominal_hypotest.root'):
             files_nominal.append(os.path.join(root, filename))
@@ -82,25 +110,8 @@ if sys.argv[1] == '-r':
         for filename in fnmatch.filter(filenames, 'Output_fixSigXSecUp_hypotest.root'):
             files_xsec_up.append(os.path.join(root, filename))
 
-    print 'Merging %i files from %s to %s' % (len(files_nominal), input_dir, output_dir)
 
-else:
-    output_dir = sys.argv[1]  
-    input_dirs  = sys.argv[2:] 
-
-    for input_dir in input_dirs:
-        for root, dirnames, filenames in os.walk(input_dir):
-            for filename in fnmatch.filter(filenames, 'Output_fixSigXSecNominal_hypotest.root'):
-                files_nominal.append(os.path.join(root, filename))
-            for filename in fnmatch.filter(filenames, 'Output_hypotest.root'):
-                files_nominal.append(os.path.join(root, filename))
-
-            for filename in fnmatch.filter(filenames, 'Output_fixSigXSecDown_hypotest.root'):
-                files_xsec_dn.append(os.path.join(root, filename))
-            for filename in fnmatch.filter(filenames, 'Output_fixSigXSecUp_hypotest.root'):
-                files_xsec_up.append(os.path.join(root, filename))
-
-    print 'Merging %i files from %s to %s' % (len(files_nominal), ','.join(input_dirs), output_dir)
+print 'Merging %i files from %s to %s' % (len(files_nominal), ','.join(input_dirs), output_dir)
 
 
 # try to guess if signal xs files exists
